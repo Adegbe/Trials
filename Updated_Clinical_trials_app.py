6da36c9d5e1dff4ae168765aba4dad41f499452c
Adegbe <67567108+Adegbe@users.noreply.github.com>
2025-06-14 17:05:59 +0100

    Update Updated_Clinical_trials_app.py

--- Updated_Clinical_trials_app.py
+++ Updated_Clinical_trials_app.py
@@ -35,11 +35,24 @@ if condition:
             id_mod = protocol.get("identificationModule", {})
             status_mod = protocol.get("statusModule", {})
             contact_mod = protocol.get("contactsLocationsModule", {})
+            sponsor_mod = protocol.get("sponsorCollaboratorsModule", {})
+            design_mod = protocol.get("designModule", {})
+            eligibility_mod = protocol.get("eligibilityModule", {})
+            refs_mod = protocol.get("referencesModule", {})
 
             trial_id = id_mod.get("nctId", "N/A")
             title = id_mod.get("briefTitle", "N/A")
             status = status_mod.get("overallStatus", "N/A")
             trial_country = contact_mod.get("locations", [{}])[0].get("country", "N/A")
+            start_date = status_mod.get("startDateStruct", {}).get("date", "N/A")
+            completion_date = status_mod.get("completionDateStruct", {}).get("date", "N/A")
+            enrollment = design_mod.get("enrollmentInfo", {}).get("count", "N/A")
+            sponsor = sponsor_mod.get("leadSponsor", {}).get("name", "N/A")
+            gender = eligibility_mod.get("sex", "N/A")
+            min_age = eligibility_mod.get("minimumAge", "N/A")
+            max_age = eligibility_mod.get("maximumAge", "N/A")
+            pub_list = refs_mod.get("referenceList", [])
+            pub_url = f"https://pubmed.ncbi.nlm.nih.gov/{pub_list[0]['PMID']}/" if pub_list else "N/A"
 
             if recruitment_status and recruitment_status.lower() not in status.lower():
                 continue
@@ -50,7 +63,15 @@ if condition:
                 "NCT ID": trial_id,
                 "Title": title,
                 "Status": status,
-                "Country": trial_country
+                "Start Date": start_date,
+                "Completion Date": completion_date,
+                "Enrollment": enrollment,
+                "Sponsor": sponsor,
+                "Gender": gender,
+                "Min Age": min_age,
+                "Max Age": max_age,
+                "Country": trial_country,
+                "Study Publication": pub_url
             })
 
         if results:
